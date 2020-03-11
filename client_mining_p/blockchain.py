fb7e80936f0c674e7bd948880f4023edb4ec6cea
heiligbasil <basilief@yahoo.com>
2020-03-11 14:41:50 -0700

    Add endpoint to retrieve last_block Increase complexity of proof by requiring more zeroes Comment out proof_of_work() method

--- client_mining_p/blockchain.py
+++ client_mining_p/blockchain.py
@@ -27,7 +27,7 @@ class Blockchain(object):
         :return: <dict> New Block
         """
         block = {
-            'index': len(self.chain) +1,
+            'index': len(self.chain) + 1,
             'timestamp': time(),
             'transactions': self.current_transactions,
             'proof': proof,
@@ -67,19 +67,19 @@ class Blockchain(object):
     def last_block(self):
         return self.chain[-1]
 
-    def proof_of_work(self, block):
-        """
-        Simple Proof of Work Algorithm
-        Stringify the block and look for a proof.
-        Loop through possibilities, checking each one against `valid_proof`
-        in an effort to find a number that is a valid proof
-        :return: A valid proof for the provided block
-        """
-        block_string = json.dumps(block, sort_keys=True)
-        proof = 0
-        while self.valid_proof(block_string, proof) is False:
-            proof +=1
-        return proof
+    # def proof_of_work(self, block):
+    #     """
+    #     Simple Proof of Work Algorithm
+    #     Stringify the block and look for a proof.
+    #     Loop through possibilities, checking each one against `valid_proof`
+    #     in an effort to find a number that is a valid proof
+    #     :return: A valid proof for the provided block
+    #     """
+    #     block_string = json.dumps(block, sort_keys=True)
+    #     proof = 0
+    #     while self.valid_proof(block_string, proof) is False:
+    #         proof += 1
+    #     return proof
 
     @staticmethod
     def valid_proof(block_string, proof):
@@ -95,7 +95,7 @@ class Blockchain(object):
         """
         guess = f'{block_string}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
-        return guess_hash[:3] == '000'
+        return guess_hash[:6] == '000000'
 
 
 # Instantiate our Node
@@ -126,6 +126,14 @@ def full_chain():
     response = {
         'chain': blockchain.chain,
         'length': len(blockchain.chain)
+    }
+    return jsonify(response), 200
+
+
+@app.route('/last_block', methods=['GET'])
+def get_last_block():
+    response = {
+        'last_block': blockchain.last_block
     }
     return jsonify(response), 200
 
