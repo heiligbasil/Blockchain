963ef8d3146bdb928cb6d8a52b2fb247cd145f92
heiligbasil <basilief@yahoo.com>
2020-03-12 13:49:18 -0700

    Initial commit

---
+++ basic_wallet_p/Blockchain_Wallet/app/src/main/java/com/lambdaschool/blockchain_wallet/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.lambdaschool.blockchain_wallet.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
